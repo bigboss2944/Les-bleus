2c9c098b43f816fedc24cb13e7349a4472766235
oni35 <oni35@hotmail.fr>
2019-12-02 18:37:52 +0100

    updatePlaydb

---
+++ PlayDB/Program.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EntitiesContext())
+            {
+                foreach (var item in db.Bicycles)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
