4399d6cb288eee87a909852e5165a52092fe99a8
fafa.noel29 <fafa.noel29@gmail.com>
2020-01-10 08:58:27 +0100

    MVVMLight UWP

---
+++ UWP_FilRouge/Entities/EntityBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_FilRouge.Entities
+{
+    public class EntityBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
