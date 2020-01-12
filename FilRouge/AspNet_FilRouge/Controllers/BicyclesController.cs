430abde119389b854251882fa33cf143062c1d0c
oni35 <oni35@hotmail.fr>
2020-01-12 01:04:32 +0100

    aspnet worked

---
+++ FilRouge/AspNet_FilRouge/Controllers/BicyclesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspNet_FilRouge.Models;
+
+namespace AspNet_FilRouge.Controllers
+{
+    public class BicyclesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Bicycles
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Bicycles.ToListAsync());
+        }
+
+        // GET: Bicycles/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bicycle bicycle = await db.Bicycles.FindAsync(id);
+            if (bicycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bicycles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,TypeOfBike,Category,Reference,FreeTaxPrice,Exchangeable,Insurance,Deliverable,Size,Weight,Color,WheelSize,Electric,State,Brand,Confort")] Bicycle bicycle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bicycles.Add(bicycle);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bicycle bicycle = await db.Bicycles.FindAsync(id);
+            if (bicycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bicycle);
+        }
+
+        // POST: Bicycles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,TypeOfBike,Category,Reference,FreeTaxPrice,Exchangeable,Insurance,Deliverable,Size,Weight,Color,WheelSize,Electric,State,Brand,Confort")] Bicycle bicycle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bicycle).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bicycle bicycle = await db.Bicycles.FindAsync(id);
+            if (bicycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bicycle);
+        }
+
+        // POST: Bicycles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            Bicycle bicycle = await db.Bicycles.FindAsync(id);
+            db.Bicycles.Remove(bicycle);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FilRouge/AspNet_FilRouge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNet_FilRouge.Startup))]
+namespace AspNet_FilRouge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
