e5e79d222489ee6c69cc82a82c05ab2bd347cbd6
cknzally <cynzally@yahoo.fr>
2020-01-13 19:06:11 +0100

    Contador3

--- FilRouge/UWP_FilRouge/Entities/Order.cs
+++ FilRouge/UWP_FilRouge/Entities/Order.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace UWP_FilRouge.Entities
 {
-    public class Order : EntityBase
+    public class Order:EntityBase 
     {
         private long idOrder;
         //quantity by reference 
@@ -170,81 +170,46 @@ namespace UWP_FilRouge.Entities
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public override object Copy()
         {
             Order order = new Order();
             order.Id = this.Id;
-            /// order.IdOrder = this.IdOrder;
-            /// order.Customer = this.Customer;
-            if (this.Customer != null)
-            {
-                order.Customer = this.Customer.Copy() as Customer;
-            }
+            order.Quantity = this.Quantity;
+            order.Customer = this.Customer;
             order.Date = this.Date;
             order.Discount = this.Discount;
-            order.PayMode = this.PayMode;
-            order.Quantity = this.Quantity;
-            /// order.Seller = this.Seller;
-            if (this.Seller != null)
-            {
-                order.Seller = this.Seller.Copy() as Seller;
-            }
-            order.ShippingCost = this.ShippingCost;
-            /// order.Shop = this.Shop;
-            if (this.Shop != null)
-            {
-                order.Shop = this.Shop.Copy() as Shop;
-            }
+            order.Seller = this.Seller;
+            order.Shop = this.Shop;
             order.SumFreeTax = this.SumFreeTax;
             order.Tax = this.Tax;
             order.TotalAmount = this.TotalAmount;
             order.UseLoyaltyPoint = this.UseLoyaltyPoint;
-            
-            return order;
+
+
+            {
+                return order;  
+            }
         }
 
         public override void CopyFrom(object obj)
         {
-            Order order = obj as Order;
+            Order order = new Order();
             order.Id = this.Id;
-            /// order.IdOrder = this.IdOrder;
-            /// order.Customer = this.Customer;
-            if (order.Customer != null)
-            {
-                this.Customer = order.Customer;
-            }
+            order.Quantity = this.Quantity;
+            order.Customer = this.Customer;
             order.Date = this.Date;
             order.Discount = this.Discount;
-            order.PayMode = this.PayMode;
-            order.Quantity = this.Quantity;
-            /// order.Seller = this.Seller;
-            if (order.Seller != null)
-            {
-                this.Seller = order.Seller;
-            }
-            order.ShippingCost = this.ShippingCost;
-            /// order.Shop = this.Shop;
-            if (order.Shop != null)
-            {
-                this.Shop = order.Shop;
-            }
+            order.Seller = this.Seller;
+            order.Shop = this.Shop;
             order.SumFreeTax = this.SumFreeTax;
             order.Tax = this.Tax;
             order.TotalAmount = this.TotalAmount;
             order.UseLoyaltyPoint = this.UseLoyaltyPoint;
-        }
 
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
             {
-                handler(this, new PropertyChangedEventArgs(name));
+                
             }
         }
-
     }
     
 }
